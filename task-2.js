f9095eb9f1ddc435b9599cdae44b2e607c31096b
Vahide Segah Üney <vahidesegah@gmail.com>
2024-10-08 20:38:47 +0300

    3333

--- task-2.js
+++ task-2.js
@@ -35,27 +35,11 @@ const getUsersWithFriend = (users, friendName) => {
   ];
   
   console.log(getUsersWithFriend(allUsers, "Briana Decker")); 
-  // [
-  //   {
-  //     name: "Sharlene Bush",
-  //     friends: ["Briana Decker", "Sharron Pace"]
-  //   },
-  //   {
-  //     name: "Sheree Anthony",
-  //     friends: ["Goldie Gentry", "Briana Decker"]
-  //   }
-  // ]
+ 
+
   
   console.log(getUsersWithFriend(allUsers, "Goldie Gentry"));
-  // [
-  //   {
-  //     name: "Elma Head",
-  //     friends: ["Goldie Gentry", "Aisha Tran"]
-  //   },
-  //   {
-  //     name: "Sheree Anthony",
-  //     friends: ["Goldie Gentry", "Briana Decker"]
-  //   }
-  // ]
+
+  
   
   console.log(getUsersWithFriend(allUsers, "Adrian Cross" ));
